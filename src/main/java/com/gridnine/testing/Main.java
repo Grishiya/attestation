8b83ccc4482090363668cf16a3bb4cba127ea737
Grisha <guruwolf8@gmail.com>
2024-01-30 00:19:26 +0300

    как понял

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -9,17 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-    List<Segment> segments;
     static List<Flight> flights;
-    Segment segment = new Segment(LocalDateTime.of(2024, 2, 4, 12, 45),
-           LocalDateTime.of( 2024, 2, 4, 16, 0));
-    Segment segment1=new Segment(LocalDateTime.of(2024, 2, 4, 11, 45),
-            LocalDateTime.of( 2024, 2, 4, 16, 0));
-    Flight flight = new Flight(segments);
-
-
-
-
     public static void main(String[] args) {
         flights = FlightBuilder.createFlights();
         System.out.println("All flights: " + flights);
